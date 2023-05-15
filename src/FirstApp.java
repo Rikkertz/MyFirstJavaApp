9046391ecb8948f8a068b063bad58456ad56f972
Rikkertz <132992098+Rikkertz@users.noreply.github.com>
2023-05-15 11:58:18 +0200

    Een klein verhaaltje geschreven

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -7,11 +7,11 @@ Written by Rik
 public class FirstApp {
     public static void main(String[] args) {
         System.out.println("Hallo Wereld!"); // This is where the magic happens!
-        System.out.print("Hallo");
-        System.out.println("Hallo");
-        System.out.println("Hallo");
-        System.out.println("Hallo");
-        System.out.println("Hallo");
+        System.out.print("Mijn naam is Rik!\n");
+        System.out.println("Ik ben 25 jaar en ik ben in opleiding tot data scientist.");
+        System.out.println("We leren nu Java en daar weet ik nog vrijwel niets van.");
+        System.out.println("Laten we er een paar leuke weken van maken!");
+        System.out.println("Houdoe!!");
 
     }
 }
