40cc7ede5575deb629dac6cf78379517aef6bee7
Rikkertz <132992098+Rikkertz@users.noreply.github.com>
2023-05-15 12:41:37 +0200

    Het verhaal in een aparte class gestoken en een lege class persoon voor de toekomst toegevoegd.

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -6,12 +6,7 @@ Written by Rik
 
 public class FirstApp {
     public static void main(String[] args) {
-        System.out.println("Hallo Wereld!"); // This is where the magic happens!
-        System.out.print("Mijn naam is Rik!\n");
-        System.out.println("Ik ben 25 jaar en ik ben in opleiding tot data scientist.");
-        System.out.println("We leren nu Java en daar weet ik nog vrijwel niets van.");
-        System.out.println("Laten we er een paar leuke weken van maken!");
-        System.out.println("Houdoe!!");
-// This is the end of this story.
+        IntroductieVerhaal verhaal = new IntroductieVerhaal();
+        System.out.println(verhaal.verhaal);
     }
 }
---
+++ src/IntroductieVerhaal.java
@@ -0,0 +1,12 @@
+/*
+This is where the story is written
+ */
+public class IntroductieVerhaal{
+
+    public String verhaal = "Hallo Wereld!\nMijn naam is Rik!\nIk ben 25 jaar en ik ben in opleiding tot data scientist.\nWe leren nu Java en daar weet ik nog vrijwel niets van.\nLaten we er een paar leuke weken van maken!\nHoudoe!!"; // This is where the magic happens!
+    // Dit is het verhaal
+    public IntroductieVerhaal() {
+        this.verhaal = verhaal;
+
+    }
+}
