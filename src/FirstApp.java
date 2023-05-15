bdf013110801ee5acf6c3e77a2da5961ae1b7238
Rikkertz <132992098+Rikkertz@users.noreply.github.com>
2023-05-15 11:51:56 +0200

    Actual first commit
    initializing project

---
+++ src/FirstApp.java
@@ -0,0 +1,17 @@
+/*
+My first java App.
+Prints 'Hallo Wereld!'
+Written by Rik
+ */
+
+public class FirstApp {
+    public static void main(String[] args) {
+        System.out.println("Hallo Wereld!"); // This is where the magic happens!
+        System.out.print("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+        System.out.println("Hallo");
+
+    }
+}
