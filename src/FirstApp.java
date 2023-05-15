fba1423a195ac7b64d3cad9236eb11377087620d
Rikkertz <132992098+Rikkertz@users.noreply.github.com>
2023-05-15 13:36:19 +0200

    Added Groetje

--- src/FirstApp.java
+++ src/FirstApp.java
@@ -7,6 +7,7 @@ Written by Rik
 public class FirstApp {
     public static void main(String[] args) {
         IntroductieVerhaal verhaal = new IntroductieVerhaal();
+        Greeter Groet = new Greeter("Harry");
         System.out.println(verhaal.verhaal);
     }
 }
---
+++ src/Greeter.java
@@ -0,0 +1,6 @@
+public class Greeter {
+
+    public Greeter(String name) {
+        System.out.println("Hello " + name + "!");
+    }
+}
